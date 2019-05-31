d5d5911c9efbd808661e2364fbf10ad1c88b8a3a
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-05-31 14:46:01 +0100

    Update build scripts

---
+++ utils/scripts/build/exports_deps1.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# LLVM/Clang should be in the PATH for this to work
+export CC=clang
+export CXX=clang++
+export LLVMCONFIG=llvm-config
+
+#COMPILER_VERSION=
+
+# or picked up from a system package install
+if [[ ! -z ${COMPILER_VERSION} ]]; then
+  export CC=${CC}-${COMPILER_VERSION}
+  export CXX=${CXX}-${COMPILER_VERSION}
+  export LLVMCONFIG=${LLVMCONFIG}-${COMPILER_VERSION}
+fi
+
+#export BUILD_TYPE=Debug
+export BUILD_TYPE=RelWithDebInfo
+
+#export GTEST_ROOT=/usr/local/gtest-libcxx
+export GTEST_ROOT=/bulk/workbench/thirdparty/gtest/install1-libcxx/
+
+#export BOOST_ROOT=/bulk/workbench/boost/015900/install/
+export BOOST_ROOT=/bulk/workbench/thirdparty/boost/016800/install1/
+
+export CXX_FLAGS=
+export CXX_FLAGS="${CXX_FLAGS} -O1"
+export CXX_FLAGS="${CXX_FLAGS} -stdlib=libc++"
+
+export LINKER_FLAGS=
+export LINKER_FLAGS="-Wl,-L$(${LLVMCONFIG} --libdir)"
+export LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi"
+
+export ITERATORRECOGNITION_SKIP_TESTS=OFF
+
+export SANITIZER_OPTIONS=""
+
+# find LLVM's cmake dir
+export LLVM_DIR=$(${LLVMCONFIG} --prefix)/share/llvm/cmake/
+
+# versions 3.8 and up provide a flag for it
+${LLVMCONFIG} --cmakedir &> /dev/null
+[[ $? -eq 0 ]] && export LLVM_DIR=$(${LLVMCONFIG} --cmakedir)
+
+
+
+CMAKE_OPTIONS="-DLLVM_DIR=${LLVM_DIR}"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DGTEST_ROOT=${GTEST_ROOT}"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DBOOST_ROOT=${BOOST_ROOT}"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DITERATORRECOGNITION_SKIP_TESTS=${ITERATORRECOGNITION_SKIP_TESTS}"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DPedigree_DIR=${PEDIGREE_DIR}"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} ${SANITIZER_OPTIONS}"
+
+export CMAKE_OPTIONS
+
